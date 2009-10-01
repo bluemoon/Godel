7e8fe466748aaa5abc64dc203b17f59c3e8a3cb9
Alex Toney <toneyalex@gmail.com>
2009-09-30 19:47:03 -0700

    removing garbage from main.py and adding a settings file, i may however change the settings to an INI file, i have yet to make up my mind

--- main.py
+++ main.py
@@ -1,59 +1,24 @@
 from optparse import OptionParser
-from containers import sentence
-from lg_test import irc_logParser
-
-from grammar_fsm import Semantics
-from rule_engine import rule_engine
-from rule_engine import test_rules
-
-
-from debug import *
-import linkGrammar
-import analysis
-import relex
-import pprint
+from settings import DATA_FOLDER
 
 class main:
     def option_parser(self):
         self.parser = OptionParser()
-        self.parser.add_option("-r", action="store_true", dest="relex")
-        self.parser.add_option("--default", action="store_true", dest="default")
+        self.parser.add_option("--rule-engine", dest="engine")
+        self.parser.add_option("--source", dest="source")
         (self.options, self.args) = self.parser.parse_args()
 
     def main(self):
-        semantics = Semantics()
-        
-        logParser = irc_logParser()
-        log_data = logParser.loadLogs('logs/2009-08-1*', limit=200)
-        
-        for sentences in log_data:
-            rule_eng  = rule_engine()
-            if not sentences:
-                continue
+        self.source = None
+        if self.options.source == 'irc-logs':
+            from input.irc_log import irc_logParser
+            
+            logParser = irc_logParser()
+            self.source = logParser.loadLogs(DATA_FOLDER + 'logs/2009-08-1*', limit=200)
 
-            if self.options.relex:
-                r = relex.relex()
-                sentence = r.process(sentences)
-                r.parse_output(sentence)
-                    
-            if self.options.default:
-
-                from tagger import braubt_tagger
-                analogy = analysis.Analogies()
-                
-                s = linkGrammar.sentence(sentences)
-                if s:
-                    normal_words = sentences.split(' ')
-                    container = sentence(s, normal_words)
-                    container.atom = semantics.semanticsToAtoms(container)
-
-                    test_rules(container)
-                    for a in container.diagram:
-                        debug(a)
-
-                    analogy.similar(container)
-
-        
+            assert len(self.source) > 0, 'irc source is invalid.'
+            
+            
 if __name__ == '__main__':
     m = main()
     m.option_parser()
