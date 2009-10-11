f216a7d26fb2fcdfbaff2584fcb3ecaab6a4993f
Alex Toney <toneyalex@gmail.com>
2009-10-11 00:48:06 -0700

    the types work now, it just has to be characterized by the hypergraph

--- analysis/rule_engine.py
+++ analysis/rule_engine.py
@@ -24,6 +24,7 @@ class rule_engine:
 
     def test_rule_parser(self):
         self.setType('$prep', 'preposition')
+        self.ground_variable('$be', 'be')
         test_rule = [['_obj','$be','$var1'], ['$prep','$var1','$var2']]
         
         self.matchRule(test_rule)
