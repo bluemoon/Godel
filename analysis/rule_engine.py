d46d1ce4ad081410ecba1d8af40465a1290b1906
Alex Toney <toneyalex@gmail.com>
2009-10-10 23:25:00 -0700

    updating the code and refactoring bits to make it work better

--- analysis/rule_engine.py
+++ analysis/rule_engine.py
@@ -1,5 +1,4 @@
 from collections import deque
-from structures.trampoline import trampoline
 from utils.debug import *
 
 import os
@@ -16,9 +15,17 @@ class rule_engine:
         self.Groundings = {}
         self.Types = {}
 
-    def run_rules(self):
+    def parse_rulefile(self, file):
         pass
     
+    def run_rules(self):
+        #self.parse_rulefile('')
+        self.test_rule_parser()
+
+    def test_rule_parser(self):
+        test_rule = [['_obj','$be','$var1'], ['$prep','$var1','$var2']]
+        self.matchRule(test_rule)
+        
     def reset(self):
         ## reset the groundings
         del self.Groundings
@@ -51,9 +58,8 @@ class rule_engine:
             return False
     
         
-    def compare_ground(self, ground, idx):
+    def compareGround(self, ground, idx):
         if self.Groundings[ground] != self.current[idx]:
-            self.last_match = False
             return False
         else:
             return True
@@ -62,19 +68,88 @@ class rule_engine:
         ## ground a specific variable
         self.Groundings[variable] = ground
     
+    def matchRule(self, rule_set):
+        results = []
+        state   = None
+        stack   = []
+        output  = []
+        
+        for matches in self.match_rule_generator(rule_set):
+            ## match to list then append the output
+            ## find and match is a generator
+            results.append(matches[0])
+            state = matches[1]
+
+        ## match it outright
+        if rule_set == results:
+            if self.preposition:
+                return (True, state, self.preposition)
+            else:
+                return (True, state)
+
+        else:
+            i = 0
+            ## if the tags > output no go, misaligned lists
+            if len(rule_set) > len(output):
+                return False
+            
+            ## FIXME: i need to fix this, so it works/doesnt suck
+            for x in tag_list:
+                print x
+                print results[i]
+                
+                if x == results[i]:
+                    output.append(True)
+                else:
+                    output.append(False)
+                    
+                i += 1
+                
+            if False in output:
+                return False
+            else:
+                return True
+        return False
+    
+    def compareRule(self, tag, var_1, var_2):
+        ## check to see if we are ground and it is a var
+        if self.isVariable(var_1) and self.isGround(var_1):
+            out = self.compareGround(var_1, 0)
+            if not out:
+                return False
+            
+        ## otherwise we need to ground it
+        elif self.isVariable(var_1) and not self.isGround(var_1):
+            self.ground_variable(var_1, self.current[0])
+     
+        if self.isVariable(var_2) and self.isGround(var_2):
+            out = self.compareGround(var_2, 2)
+            if not out:
+                return False
+            
+        elif self.isVariable(var_2) and not self.isGround(var_2):
+            self.ground_variable(var_2, self.current[2])
+
+        if tag == '$prep':
+            ## XXX: bad way of doing this
+            return True
+        
+        if self.isVariable(tag) and self.isGround(tag):
+            self.compare_ground(tag, 1)
+                
+        elif self.isVariable(var_2) and not self.isGround(var_2):
+            self.ground_variable(tag, self.current[1])
+            
+        return True
     
     def find_next(self, tag):
         ## change this to istype and isvariable
-        if isType(tag):
-            tagType = getType(tag)
+        if self.isType(tag):
+            tagType = self.getType(tag)
             for x in self.hypergraph.edge_by_type(tagType):
                 head, tag, tail = x
                 edge_data = tag[0]
-                
-                for preps in prepositions:
-                    if edge_data in prepositions:
-                        self.preposition = preps
-                        yield (head, edge_data, tail)
+                yield (head, edge_data, tail)
             
             yield False            
             return
@@ -96,10 +171,11 @@ class rule_engine:
                     self.current = (head, tag[0], tail)
                     yield self.current
                     
-    def match_rule(self, rule_set):
+    def match_rule_generator(self, rule_set):
         ## tag list is the list we match to
         ## it is the rule
         rule_set = deque(rule_set)
+        debug(rule_set)
         
         ## has to be a deque with something in it
         if rule_set:
@@ -112,20 +188,20 @@ class rule_engine:
             for x in self.find_next(tag):
                 if x:
                     ## find our matching tag
-                    match = self.match_rule(tag, var_1, var_2)
+                    match = self.compareRule(tag, var_1, var_2)
                     if match:
                         self.match_stack.append(self.groundings)
                         yield ([tag, var_1, var_2], self.groundings)
 
                         ## run with recursion after yield
-                        for x in self.match_rule(rule_set):
+                        for x in self.match_rule_generator(rule_set):
                             ## check for sanity's sake
                             if len(tag_list) > 0:
                                 tag, var_1, var_2 = rule_set.popleft()
                             else:
                                 return
                             #debug((tag, var_1, var_2))
-                            match = self.match_rule(tag, var_1, var_2)
+                            match = self.matchRule(tag, var_1, var_2)
                             #debug(match)
                             if match:
                             ## dump on the stack
